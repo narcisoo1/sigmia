6820b3c0b8df4955abbd7c814452899bda3645cb
narciso.sousa <narciso.sousa@aliger.io>
2023-07-25 15:58:42 -0300

    ajuste para o sistema não seguir cor do SO

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from PyQt5 import QtWidgets
 from controllers.MainController import ApplicationWindow
+from PyQt5.QtWidgets import QStyleFactory
+from PyQt5.QtCore import Qt
 import sys
 
 
+def set_light_palette(app):
+    # Defina a paleta de cores da aplicação como uma paleta clara
+    palette = app.palette()
+    palette.setColor(palette.Window, Qt.white)  # Cor do fundo da janela
+    palette.setColor(palette.WindowText, Qt.black)  # Cor do texto da janela
+    # Defina outras cores de acordo com suas necessidades
+
+    app.setPalette(palette)
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('GTK'))
+    set_light_palette(app)
     application = ApplicationWindow()
 
     application.show()
